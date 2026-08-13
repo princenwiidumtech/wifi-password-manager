30b4683db9e0cae5c66f4702f4383b665ffc70fd
Nwiidum Prince Deedum <nwiidumprince.tech@gmail.com>
2026-08-13 10:43:39 +0100

    Refactor routes to use templates and add view route
    Updated home and save routes to render templates. Added a new view route to display saved Wi-Fi credentials.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
-from wifi_manager import save_wifi
+from wifi_manager import save_wifi, view_wifi
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Wi-Fi Password Manager is running!"
+    return render_template("index.html")
 
 
 @app.route("/save", methods=["GET", "POST"])
@@ -17,9 +17,16 @@ def save():
 
         save_wifi(ssid, password)
 
-        return "Wi-Fi saved successfully!"
+        return render_template("save.html")
 
     return render_template("save.html")
+
+
+@app.route("/view")
+def view():
+    wifi_list = view_wifi()
+
+    return render_template("view.html", wifi_list=wifi_list)
 
 
 if __name__ == "__main__":
