5e0fb437ccc71b98a4d0b3d1e9648200b15b00ef
Nwiidum Prince Deedum <nwiidumprince.tech@gmail.com>
2026-08-27 15:17:43 +0100

    Add password hashing functions from Werkzeug

--- wifi_manager.py
+++ wifi_manager.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 from encryption import encrypt_password, decrypt_password
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Create database folder if it doesn't exist
 os.makedirs("database", exist_ok=True)
