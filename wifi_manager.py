d036819bd9601ef1d4c542078d18112463fd0b61
Nwiidum Prince Deedum <nwiidumprince.tech@gmail.com>
2026-09-04 14:09:55 +0100

    Fix connection close method in wifi_manager.py

--- wifi_manager.py
+++ wifi_manager.py
@@ -29,9 +29,7 @@ CREATE TABLE IF NOT EXISTS users (
 """)
 
     conn.commit()
-    conn.close()
-
-def init_database():
+    conn.close)
 
 def save_wifi(ssid, password):
     conn = sqlite3.connect(DB_PATH)
